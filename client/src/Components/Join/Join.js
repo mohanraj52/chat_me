310c770df68bad7e579c72c0cea6d155683fd1ac
mohanraj52 <mohandhanush360@gmail.com>
2021-06-11 09:40:18 +0530

    changes

--- client/src/Components/Join/Join.js
+++ client/src/Components/Join/Join.js
@@ -30,7 +30,7 @@ const Join = () => {
         </div>
         <Link
           onClick={(event) => {
-            !name || !room ? event.preventDefault() : null;
+            return !name || !room ? event.preventDefault() : null;
           }}
           to={`/chat?name=${name}&room=${room}`}
         >
